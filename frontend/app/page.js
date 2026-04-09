09608a979d81193951ec0cfef09f97cf225dbe09
Oleg Pelin <pelinoleg@gmail.com>
2026-04-09 19:10:14 +0200

    test webhook 2

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl">
-        Portfolio
+        Portfolio2
       </main>
     </div>
   );
